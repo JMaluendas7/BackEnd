af0d39cc025de431326461b326a4cf89d2751623
JMaluendas <jmaluendase@gmail.com>
2023-11-21 15:02:48 -0500

    Envio automatico de correos

--- myapp/utils.py
+++ myapp/utils.py
@@ -1,72 +1,67 @@
 
 import pyodbc
 import smtplib
-import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
 # Función para enviar correo electrónico
+
+
 def send_email(identificacion, nombre, apellido, correo):
+
     # Configurar el correo electrónico
-    email_origen = "jmaluendase@gmail.com"
-    email_destino = 'jmaluendasbautista@gmail.com'
+    email_origen = "asistemas@berlinasdelfonce.com"
+    email_destino = "jmaluendase@gmail.com"
     ruta_img = "https://saas-cms-admin-sandbox.s3.us-west-2.amazonaws.com/sites/647e59513d04a300028afa72/assets/647e59b33d04a300028afa77/Logo_berlinas_blanco_fondo-transparente_DIGITAL.png"
-    ruta_img2 = "http://gestor.berlinasdelfonce.com/img/iconlogobus.83dc07f2.png"
 
     message = MIMEMultipart()
     message["From"] = email_origen
-    message["To"] = email_destino
+    message["To"] = "jmaluendase@gmail.com"
     message["Subject"] = "Portal Propietarios - Envio Link de tutoriales"
 
     body = f"""
         <!DOCTYPE html>
-        <html lang="en">
+        <html lang="es">
         <head>
             <meta charset="UTF-8">
             <title>Portal Propietarios - Envio Link de tutoriales</title>
         </head>
-        <body style="font-family: Arial, sans-serif;">
-            <div style="max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 10px; background-color: #fff; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);">
-                <div style="display: flex; justify-content: space-around; background-color: #009944; height: auto; width: 100%; border-bottom-left-radius: 1rem; border-bottom-right-radius: 1rem;">
-                    <img src={ruta_img} alt="Logo de la empresa" style="max-width: 150px; display: block; ">
-                    <img src={ruta_img} alt="Logo de la empresa" style="max-width: 150px; display: block; ">
+        <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
+            <div style="max-width: 700px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 10px; background-color: #fff; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);">
+                <div style="text-align: center; background-color: #009944; border-radius: 10px;">
+                    <img src="{ruta_img}" alt="Logo de la empresa" style="max-width: 150px; display: block; margin: 20px auto;">
                 </div>
-                <div style="margin-top: 20px; line-height: 1.6;">
-                    <p>Estimado/a {nombre} {apellido},</p>
-                    <p>Esperamos que este mensaje le encuentre bien. En Berlinas del Fonce S.A., nos complace presentarle nuestros video tutoriales diseñados especialmente para quienes hacen uso de nuestro sistema de información, el Portal de Propietarios.</p>
-                    <p>Estos tutoriales están diseñados específicamente para guiarle a través de las diversas funciones del Portal. Desde cómo acceder al sistema hasta la consulta de extractos, análisis detallado de las estadísticas de sus buses y la descarga de certificados cruciales para su gestión, estos recursos están creados para brindarle la orientación necesaria en cada paso.</p>
-                    <p>Puede acceder a los video tutoriales siguiendo este <a style="text-decoration: none; color: #4caf50;" href="https://drive.google.com/file/d/1NwbuLAv6IUCQgiXwTFZIdSg-Ujxh467M/view">enlace</a>, donde encontrará información detallada sobre cómo aprovechar al máximo las funcionalidades que el sistema Portal tiene para usted.</p>
-                    <p>Si tiene alguna pregunta o necesita asistencia adicional, no dude en ponerse en contacto con nuestro equipo de soporte técnico al </a><br>Teléfono: <a style="text-decoration: none;" href="https://api.whatsapp.com/send?phone=+573168756931">3168756931</a> o <a style="text-decoration: none;" href="mailto:asistemas@berlinasdelfonce.com">asistemas@berlinasdelfonce.com</a>.</p>
-
+                <div style="margin-top: 20px; line-height: 1.6; color: #000;">
+                    <p>Estimado(a) propietario(a) {nombre} {apellido},</p>
+                    <p color: #000 !important;>De antemano, reciba un cordial saludo de Berlinas del Fonce S.A., nos complace presentarle los videotutoriales donde encontrará información detallada sobre cómo aprovechar al máximo las funcionalidades que ofrece el nuevo <strong style="color: #4caf50;">Portal de Propietarios</strong>; desde como acceder al sistema hasta la consulta de extractos, descarga de certificados y el análisis estadístico de gastos e ingresos de sus vehículos.</p>
+                    <p color: #000 !important;>Puede acceder al videotutorial siguiendo el siguiente <a style="text-decoration: none; color: #4caf50;" href="https://drive.google.com/file/d/1NwbuLAv6IUCQgiXwTFZIdSg-Ujxh467M/view">enlace</a>.</p>
+                    <p color: #000 !important;>A continuación el link de acceso al portal y su respectivo usuario<br>
+                    Link de acceso: <a style="text-decoration: none; color: #4caf50;" href="gestor.berlinasdelfonce.com">gestor.berlinasdelfonce.com</a><br>
+                    Usuario: {identificacion}</p>
+                    <p color: #000 !important;><strong>Nota</strong>: Los extractos de noviembre del 2023 no serán enviados vía correo electrónico, por lo tanto, deberán ser consultarlos en el nuevo portal de propietarios.</p>
+                    <p color: #000 !important;>Para cualquier inconveniente o consulta adicional, por favor, comunicarse con el Sr. Jorge Maluendas, quien está a cargo de brindarle asistencia técnica y puede contactarlo al teléfono <a style="text-decoration: none; color: #4caf50;" href="https://api.whatsapp.com/send?phone=+573168756931">3168756931</a> o escribiéndole al correo <a style="text-decoration: none; color: #4caf50;" href="mailto:asistemas@berlinasdelfonce.com">asistemas@berlinasdelfonce.com</a>.</p>
                     <hr style="border: 0; border-top: 1px solid #ccc; margin: 20px 0;">
-                    <p style="color: #333;"><strong>Cordialmente,</strong></p>
-                    <p>Jorge Eliecer Maluendas Bautista<br>Programador de Sistemas<br><a style="text-decoration: none; color: #000;" href="mailto:asistemas@berlinasdelfonce.com">asistemas@berlinasdelfonce.com</a><br>Teléfono: <a style="text-decoration: none; color: #000;" href="https://api.whatsapp.com/send?phone=+573168756931">3168756931</a><br>Cra. 68D No. 15 – 15<br>Bogotá D.C. - Colombia</p>
+                    <p style="color: #009944;"><strong>Cordialmente,</strong></p>
+                    <p color: #000 !important;>Jorge Eliecer Maluendas Bautista<br>Programador de Sistemas<br><a style="text-decoration: none; color: #009944;" href="mailto:asistemas@berlinasdelfonce.com">asistemas@berlinasdelfonce.com</a><br>Teléfono: <a style="text-decoration: none; color: #009944;" href="https://api.whatsapp.com/send?phone=+573168756931">3168756931</a><br>Cra. 68D No. 15 – 15<br>Bogotá D.C. - Colombia</p>
                 </div>
-                <div style="margin-top: 30px; font-style: italic; font-size:10px; color: #888;">
+                <div style="margin-top: 30px; font-style: italic; font-size: 10px; color: #888; text-align: center;">
                     <p>Berlinas del Fonce S.A.</p>
                 </div>
             </div>
         </body>
         </html>
     """
-    # Sustituir los placeholders {nombre} y {apellido} en el cuerpo del correo
-    body = body.replace("{nombre}", nombre)
-    body = body.replace("{apellido}", apellido)
 
     message.attach(MIMEText(body, "html"))
 
-    # Conexión SMTP y enviar el correo electrónico
-    context = ssl.create_default_context()
-    smtp_host = 'sandbox.smtp.mailtrap.io'
-    smtp_port = 2525
-
-    # Configurar el mensaje y el servidor SMTP
-    with smtplib.SMTP(smtp_host, smtp_port) as server:
-        server.starttls(context=context)
-        server.login('9b86b787587574', 'b087222934202f')
-        server.send_message(message)
-    print(f"Correo enviado a {email_destino}")
+    try:
+        smtp = smtplib.SMTP_SSL("mail.berlinasdelfonce.com")
+        smtp.login(email_origen, "PRUEBA2023")
+        smtp.sendmail(email_origen, email_destino, message.as_string())
+        smtp.quit()
+        print("Correo Enviado")
+    except Exception as e:
+        print(f"No se pudo enviar el correo a {correo} {str(e)}")
 
 
 # Parámetros de conexión a la base de datos
@@ -74,6 +69,7 @@ server = '172.16.0.25'
 database = 'DynamiX'
 username = 'developer'
 password = '123456'
+
 
 # Conectarse a la base de datos y ejecutar la consulta SQL
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server +
